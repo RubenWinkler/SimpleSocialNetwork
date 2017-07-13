8b1ad8c92f4f528d41fd9bc5008c1675217eafe0
RubenWinkler <r.winkler1412@gmail.com>
2017-07-13 03:43:46 +0200

    wefwefwef
    wefwefwef

--- src/assets/widgets/chat-widget/chat-widget.js
+++ src/assets/widgets/chat-widget/chat-widget.js
@@ -1,8 +1,8 @@
 //kann ein Element aus dem Array entfernen
 Array.remove = function(array, from, to) {
-    var rest = array.slice((to || from) + 1 || array.length);
-    array.length = from < 0 ? array.length + from : from;
-    return array.push.apply(array, rest);
+   var rest = array.slice((to || from) + 1 || array.length);
+   array.length = from < 0 ? array.length + from : from;
+   return array.push.apply(array, rest);
 };
 
 //definiert die maximale Anzahl an Pop-Ups in Abhängigkeit vom Viewport
@@ -13,76 +13,76 @@ var popups = [];
 
 //schließt ein Pop-Up
 function close_popup(id) {
-    for(var iii = 0; iii < popups.length; iii++) {
-        if(id == popups[iii]) {
-            Array.remove(popups, iii);
-            document.getElementById(id).style.display = "none";
-            calculate_popups();
-            return;
-        }
-    }
+   for(var iii = 0; iii < popups.length; iii++) {
+       if(id == popups[iii]) {
+           Array.remove(popups, iii);
+           document.getElementById(id).style.display = "none";
+           calculate_popups();
+           return;
+       }
+   }
 }
 
 //zeigt die Pop-Up's an - Anzahl der Pop-Up's ist abhängig von der maximalen Anzahl an Pop-Up's, die mit dem aktuellen Viewport angezeigt werden können
 function display_popups() {
-    var right = 220;
+   var right = 220;
 
-    var iii = 0;
-    for(iii; iii < total_popups; iii++) {
-        if(popups[iii] != undefined) {
-            var element = document.getElementById(popups[iii]);
-            element.style.right = right + "px";
-            right = right + 320;
-            element.style.display = "block";
-        }
-    }
+   var iii = 0;
+   for(iii; iii < total_popups; iii++) {
+       if(popups[iii] != undefined) {
+           var element = document.getElementById(popups[iii]);
+           element.style.right = right + "px";
+           right = right + 320;
+           element.style.display = "block";
+       }
+   }
 
-    for(var jjj = iii; jjj < popups.length; jjj++) {
-        var element = document.getElementById(popups[jjj]);
-        element.style.display = "none";
-    }
+   for(var jjj = iii; jjj < popups.length; jjj++) {
+       var element = document.getElementById(popups[jjj]);
+       element.style.display = "none";
+   }
 }
 
 //generiert den HTML-Code für neue Pop-Up's und fügt die Pop-Up-ID dem Array hinzu
 function register_popup(id, name) {
 
-    //$(".chat_form").submit();
+   //$(".chat_form").submit();
 
-    for(var iii = 0; iii < popups.length; iii++) {
-        //wenn ID schon bekannt, bringe Pop-Up nach vorne
-        if(id == popups[iii]) {
-            Array.remove(popups, iii);
-            popups.unshift(id);
-            calculate_popups();
-            return;
-        }
-    }
+   for(var iii = 0; iii < popups.length; iii++) {
+       //wenn ID schon bekannt, bringe Pop-Up nach vorne
+       if(id == popups[iii]) {
+           Array.remove(popups, iii);
+           popups.unshift(id);
+           calculate_popups();
+           return;
+       }
+   }
 
-    var current_user = $("#username").html();
+   var current_user = $("#username").html();
 
-    var element = '<div class="popup-box chat-popup" id="'+ id +'">';
-    element = element + '<div class="popup-head">';
-    element = element + '<div class="popup-head-left">'+ name +'</div>';
-    element = element + '<form class="chat-ident-form" action="javascript:void(0);" method="post"><input type="hidden" name="to_user" value="'+ name +'"><input type="hidden" name="from_user" value="'+ current_user +'"></form> '
-    element = element + '<div class="popup-head-right"><a href="javascript:close_popup(\''+ id +'\');">&#10005;</a></div>';
-    element = element + '<div style="clear: both"></div></div><div class="popup-messages"><div class="chat-container" id="'+ id + "-chat-container"+'"></div>';
-    element = element + '<form class="form-group chat-box-form" method="POST" action="./src/assets/widgets/chat-widget/send-messages.php" onsubmit="submit_via_ajax();"><input class="form-control chat-input" name="chat_message"><input type="hidden" name="chat_target_user" value="'+ name +'"><input type="hidden" name="chat_user" value="'+ current_user +'"><input class="form-control chat-submit-button" type="submit" name="senden"  value="Senden"></form></div></div>';
-    document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML + element;
-    popups.unshift(id);
-    calculate_popups();
+   var element = '<div class="popup-box chat-popup" id="'+ id +'">';
+   element = element + '<div class="popup-head">';
+   element = element + '<div class="popup-head-left">'+ name +'</div>';
+   element = element + '<form class="chat-ident-form" action="javascript:void(0);" method="post"><input type="hidden" name="to_user" value="'+ name +'"><input type="hidden" name="from_user" value="'+ current_user +'"></form> '
+   element = element + '<div class="popup-head-right"><a href="javascript:close_popup(\''+ id +'\');">&#10005;</a></div>';
+   element = element + '<div style="clear: both"></div></div><div class="popup-messages"><div class="chat-container" id="'+ id + "-chat-container"+'"></div>';
+   element = element + '<form class="form-group chat-box-form" method="POST" action="./src/assets/widgets/chat-widget/send-messages.php" onsubmit="submit_via_ajax();"><input class="form-control chat-input" name="chat_message"><input type="hidden" name="chat_target_user" value="'+ name +'"><input type="hidden" name="chat_user" value="'+ current_user +'"><input class="form-control chat-submit-button" id="'+id+'-form-control" type="submit" name="senden"  value="Senden"></form></div></div>';
+   document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML + element;
+   popups.unshift(id);
+   calculate_popups();
 }
 
 //berechnet die maximale Anzahl an Pop-Up's und speichert den Wert in die "total_popups"-Variable (s.o.)
 function calculate_popups() {
-    var width = window.innerWidth;
-    if(width < 540) {
-        total_popups = 0;
-    } else {
-        width = width - 200;
-        //320 ist die Breite eines einzelnen Pop-Up's
-        total_popups = parseInt(width/320);
-    }
-    display_popups();
+   var width = window.innerWidth;
+   if(width < 540) {
+       total_popups = 0;
+   } else {
+       width = width - 200;
+       //320 ist die Breite eines einzelnen Pop-Up's
+       total_popups = parseInt(width/320);
+   }
+   display_popups();
 }
 
 //berechnet Anzahl an Pop-Up's neu, sobald Seite neu geladen wird oder Fenster skaliert wird
@@ -90,57 +90,43 @@ window.addEventListener("resize", calculate_popups);
 window.addEventListener("load", calculate_popups);
 
 function submit_via_ajax () {
-  $(this).ajaxForm(function() {
-     $(".chat-box-form").clearForm();
-   });
+ $(this).ajaxForm(function() {
+    $(".chat-box-form").clearForm();
+  });
 }
-
-function submit_ident_form_via_ajax () {
-   $(".chat-ident-form").ajaxSubmit(function() {
-     getMessages();
-    });
-}
-
-$(document).ready(function() {
-
-    $(document).on('click', '.chat-ident-form', function(){
-      var to_user = $("#user").html();
-    });
-});
 
 function getMessages (clicked_user, current_user) {
 
-  var clicked = clicked_user;
-  var current = current_user;
+ $(document).on('click', '#'+clicked_user+'-form-control', function(){
+       syncMessages();
+ });
 
-  function syncMessages () {
+ function syncMessages () {
 
-  console.log("WURKS", clicked_user, current_user);
+ console.log("WURKS", clicked_user, current_user);
 
-  var request = new XMLHttpRequest();
+ var request = new XMLHttpRequest();
 
-  var requestURL = './src/assets/widgets/chat-widget/get-messages.php?from_user='+current_user+'&to_user='+clicked_user;
+ var requestURL = './src/assets/widgets/chat-widget/get-messages.php?from_user='+current_user+'&to_user='+clicked_user;
 
-  request.onreadystatechange = function () {
+ request.onreadystatechange = function () {
 
-    if (request.readyState == 4 && request.status == 200) {
+   if (request.readyState == 4 && request.status == 200) {
 
-      $(document).ready(function() {
+     $(document).ready(function() {
 
-          $("#"+clicked_user+"-chat-container").html(request.responseText);
-      });
+         $("#"+clicked_user+"-chat-container").html(request.responseText);
+     });
+   }
+ };
 
-    }
+ request.open('GET', requestURL , true);
 
-  };
+ request.send();
 
-  request.open('GET', requestURL , true);
+ }
 
-  request.send();
+ syncMessages();
+ setInterval(function(){syncMessages ();},3000);
 
-  }
-
-  syncMessages();
-  setInterval(function(){syncMessages ();},3000);
-
-}
+ }
